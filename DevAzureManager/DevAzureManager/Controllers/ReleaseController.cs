51f05841d83d890eff57863d766043742d5fc4b3
Vittoria Zago <vittoriazago1@hotmail.com>
2019-10-11 19:49:22 -0300

    Creating base classes

---
+++ DevAzureManager/DevAzureManager/Controllers/ReleaseController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace DevAzureManager.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ReleaseController : ControllerBase
+    {
+        private readonly ILogger<ReleaseController> _logger;
+
+        public ReleaseController(ILogger<ReleaseController> logger)
+        {
+            _logger = logger;
+        }
+
+    }
+}
